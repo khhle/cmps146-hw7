05fda21aba60e6cb20fbb3237bc60166ffe56a0b
khhle <khhle@ucsc.edu>
2015-05-21 02:54:11 -0700

    Finish step 1 - step 6

---
+++ p7_visualize.py
@@ -0,0 +1,56 @@
+import json
+import sys
+
+
+
+def parse_json_result(out):
+    """Parse the provided JSON text and extract a dict
+    representing the predicates described in the first solver result."""
+    with open(out) as f:
+        result = json.loads(out)
+    
+    assert len(result['Call']) > 0
+    assert len(result['Call'][0]['Witnesses']) > 0
+    
+    witness = result['Call'][0]['Witnesses'][0]['Value']
+    
+    class identitydefaultdict(collections.defaultdict):
+        def __missing__(self, key):
+            return key
+    
+    preds = collections.defaultdict(set)
+    env = identitydefaultdict()
+    
+    for atom in witness:
+        if '(' in atom:
+            left = atom.index('(')
+            functor = atom[:left]
+            arg_string = atom[left:]
+            try:
+                preds[functor].add( eval(arg_string, env) )
+            except TypeError:
+                pass # at least we tried...
+            
+        else:
+            preds[atom] = True
+    
+    return dict(preds)
+
+
+
+def render_ascii_dungeon(design):
+    """Given a dict of predicates, return an ASCII-art depiction of the a dungeon."""
+    
+    sprite = dict(design['sprite'])
+    param = dict(design['param'])
+    width = param['width']
+    glyph = dict(space='.', wall='W', altar='a', gem='g', trap='_')
+    block = ''.join([''.join([glyph[sprite.get((r,c),'space')]+' ' for c in range(width)])+'\n' for r in range(width)])
+    return block
+
+# Render the example thingie to the screen
+with open(sys.argv[1]) as myfile:
+    data = myfile.read().replace('\n', '')
+	
+design = parse_json_result(data)
+print render_ascii_dungeon(design)
